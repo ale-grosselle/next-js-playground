52dfd53b73d8f4dabd9afb0fda802e7be57c3b20
Alessandro Grosselle <alessandro.grosselle@adevinta.com>
2024-11-11 11:28:39 +0100

    change google url

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         },
     },
     use: {
-        baseURL: 'https://www.google.it',
+        baseURL: 'https://www.google.com',
         headless: true,
     },
     webServer: {
